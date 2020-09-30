a1d8df9180d26a6991e50809f0bf4d797326b5f2
Lune00 <paul.schuhm@gmail.com>
2020-09-30 23:03:49 +0200

    add save_person action in toolbar

--- source/app/src/main/java/com/schuhmacher/activities/AddPersonActivity.java
+++ source/app/src/main/java/com/schuhmacher/activities/AddPersonActivity.java
@@ -1,6 +1,9 @@
 package com.schuhmacher.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
@@ -17,5 +20,27 @@ public class AddPersonActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_person_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_save_person:
+                savePerson();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void savePerson() {
+        Log.i("button", "Save person !");
     }
 }
